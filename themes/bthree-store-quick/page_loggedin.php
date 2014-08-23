1fcbf12e20edbd2d4ceeae42431265f4833e4df6
bartv42 <bart.veldhuizen@gmail.com>
2014-08-23 09:17:18 -0400

    Fixes layout

--- themes/bthree-store-quick/page_loggedin.php
+++ themes/bthree-store-quick/page_loggedin.php
@@ -37,8 +37,6 @@ get_header(); ?>
 							'message'  => __( 'If you have shopped with us before, please enter your details in the boxes below. If you are a new customer please proceed to the Billing &amp; Shipping section.', 'woocommerce' ),
 							'redirect' => get_permalink( wc_get_page_id( 'checkout' ) ),
 							'hidden'   => false
-
-
 						)
 					);
 
